8acb0511191ea0cf1e44554367ddae9ac0bd4945
michealnaita <michealnaita@gmail.com>
2023-01-02 20:18:19 +0300

    feat: gets parameter data via dialogflow webhook

--- app/src/routes/dialogflow.ts
+++ app/src/routes/dialogflow.ts
@@ -1,5 +1,17 @@
-import { Router, Response, Request } from 'express';
+import express, { Router, Response, Request } from 'express';
 
 const route: Router = Router();
 
+route.use(express.json());
+
+route.post('/', (req: Request, res: Response) => {
+  try {
+    const donationItem = req.body.queryResult.parameters.item;
+    console.log(donationItem);
+    res.status(400).send('use default response');
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('internal server error');
+  }
+});
 export default route;
